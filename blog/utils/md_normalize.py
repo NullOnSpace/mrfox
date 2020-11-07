261de51eb6144b950c3529ec5f038c030393664a
NullOnSpace <portuguese_d029@163.com>
2020-11-07 19:43:09 +0800

    update md normalize to distinguish code block more easy

--- blog/utils/md_normalize.py
+++ blog/utils/md_normalize.py
@@ -4,24 +4,34 @@ from textwrap import dedent
 from io import StringIO
 
 def normalize(fp):
-    block = "normal"
+    block = "new block"
     lines = list()
     for line in fp:
         ignore = False
+        prepend = 0
+        append = 0
         if block == "quoted code block":
             if line.strip().startswith("```"):
-                block = "normal"
+                block = "new block"
+                append = 2
                 ignore = True
             else:
                 line = "    "+line
         elif block == "indent code block":
             pass
         else:
+            block = "normal"
             if line.strip().startswith("#"):
                 # title
                 pass
             elif line.strip().startswith("```"):
                 ignore = True
+                if block == "one blank":
+                    prepend = 1
+                elif block == "new block":
+                    prepend = 0
+                else:
+                    prepend = 2
                 block = "quoted code block"
             elif line.strip().startswith("> "):
                 # reference
@@ -33,14 +43,18 @@ def normalize(fp):
                 # ul
                 pass
             elif not line.strip():
-                # empty line
-                pass
+                if block == "one blank":
+                    block = "new block"
+                elif block == "new block":
+                    ignore = True
             else:
                 # normal line
                 pass
             line = line.rstrip() + "  \n"
+        lines.append("  \n"*prepend)
         if not ignore:
             lines.append(line)
+        lines.append("  \n"*append)
     return "".join(lines)
 
 def main():
