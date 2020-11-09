60a5bff7e029b8d02c68b9084d619d852d483c82
NullOnSpace <portuguese_d029@163.com>
2020-11-09 15:42:04 +0800

    Enhance markdown

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 ### features
 - [x] **ft001** tagging
 - [ ] show preview of md while editing
+- [ ] enhance markdown
 
 ### bugs
 - [x] users can edit other users' blog through corresponding url
--- blog/utils/md_normalize.py
+++ blog/utils/md_normalize.py
@@ -5,51 +5,70 @@ from io import StringIO
 
 def normalize(fp):
     block = "new block"
+    parent_block = ""
+    indent_level = 0
     lines = list()
     for line in fp:
         ignore = False
         prepend = 0
         append = 0
+        last_indent_level = indent_level
+        indent_level = ceil(len(line)-len(line.lstrip()) / 4)
         if block == "quoted code block":
             if line.strip().startswith("```"):
                 block = "new block"
-                append = 2
+                if parent_block:
+                    append = 1
+                else:
+                    append = 2
                 ignore = True
             else:
                 line = "    "+line
         elif block == "indent code block":
             pass
         else:
-            block = "normal"
             if line.strip().startswith("#"):
-                # title
-                pass
+                block = "new block"
+                parent_block = ""
+                append = 1
             elif line.strip().startswith("```"):
                 ignore = True
                 if block == "one blank":
                     prepend = 1
                 elif block == "new block":
                     prepend = 0
+                elif block.endswith("list"):
+                    prepend = 1
+                    parent_block = block
                 else:
                     prepend = 2
                 block = "quoted code block"
             elif line.strip().startswith("> "):
                 # reference
-                pass
+                block = "reference"
             elif re.search(r"^\s+\d+\.", line):
                 # ol
-                pass
+                if block != "new block" and not block.endswith("list"):
+                    prepend = 1
+                block = "ordered list"
             elif re.search(r"^\s+- ", line):
                 # ul
-                pass
+                if block != "new block" and not block.endswith("list"):
+                    prepend = 1
+                block = "unordered list"
             elif not line.strip():
                 if block == "one blank":
+                    parent_block = ""
                     block = "new block"
                 elif block == "new block":
+                    parent_block = ""
                     ignore = True
+                else:
+                    parent_block = block
+                    block = "one blank"
             else:
                 # normal line
-                pass
+                block = "normal"
             line = line.rstrip() + "  \n"
         lines.append("  \n"*prepend)
         if not ignore:
@@ -60,13 +79,21 @@ def normalize(fp):
 def main():
     lines = """\
     # h1
+    -   this is a unordered
+    -   this is another unordered
     ```
     def function(args):
         code line1
         code line2
     ```
-    1. ol
-    2. ol
+    1.  ol
+    2.  ol
+    -   this is a level 1 ul
+        -   this is level 2 ul for test bock
+            ```
+            def function():
+                pass
+            ```
     """
     dedent(lines)
     print(normalize(StringIO(lines)))
